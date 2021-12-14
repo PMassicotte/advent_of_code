e4f9b6ae09672667585aa6e5f733b315b99874bb
Philippe Massicotte <pmassicotte@hotmail.com>
2021-12-14 13:45:11 -0500

    Work on day 14.

--- day14.R
+++ day14.R
@@ -3,7 +3,8 @@ library(here)
 
 file <- "~/Desktop/input"
 
-template <- read_lines(file, n_max = 1)
+# file <- here("data", "input", "2021", "14_input")
+polymer <- read_lines(file, n_max = 1)
 
 rules <- read_delim(file, skip = 2, col_names = c("pair", "insert"), delim = " -> ")
 
@@ -28,18 +29,64 @@ insert_element <- function(pair) {
 }
 
 combine_element <- function(elements) {
-
   elements <- res
-
   n <- length(elements)
-
-
   paste0(elements)
-
 }
 
-polymer <- split_pair(template)
+for (i in 1:4) {
+  # print(i)
+  polymer <- split_pair(polymer)
 
-res <- map(polymer, insert_element)
+  res <- map(polymer, insert_element)
 
-reduce(res, )
+  polymer <- reduce(res, ~ paste0(..1, str_sub(..2, 2, -1)))
+
+  print(polymer)
+}
+
+element_count <- table(strsplit(polymer, ""))
+max(element_count) - min(element_count)
+
+
+# Part 2 ------------------------------------------------------------------
+
+options(digits = 20)
+
+file <- "~/Desktop/input"
+
+# file <- here("data", "input", "2021", "14_input")
+polymer <- read_lines(file, n_max = 1)
+
+rules <- read_delim(
+  file,
+  skip = 2,
+  col_names = c("pair", "insert"),
+  delim = " -> "
+)
+
+lut <- rules %>%
+  mutate(
+    left = paste0(str_sub(pair, 1, 1), insert),
+    right = paste0(insert, str_sub(pair, 2, 2))
+  ) %>%
+  pivot_longer(c("left", "right")) %>%
+  select(pair, value)
+
+# Initial count
+df_count <- rules %>%
+  mutate(n = str_count(polymer, pair)) %>%
+  filter(n > 0)
+
+df_count
+
+for (i in 1:1) {
+  df_count <- df_count %>%
+    full_join(lut, by = "pair") %>%
+    group_by(pair = value) %>%
+    summarise(n = sum(n, na.rm = TRUE))
+}
+
+df_count
+
+
